bd222c487b959d3dce4e85ecdaad72593bfa783b
Cullan Carey <cullancarey@gmail.com>
2025-04-29 20:37:22 -0400

    Refactor CloudFront security headers; improve content security policy formatting for better readability and maintainability.

--- src/my_constructs/cloudfront_distribution.py
+++ src/my_constructs/cloudfront_distribution.py
@@ -68,7 +68,16 @@ class CloudfrontDistribution(Construct):
                 security_headers_behavior=cloudfront.ResponseSecurityHeadersBehavior(
                     content_security_policy=cloudfront.ResponseHeadersContentSecurityPolicy(
                         content_security_policy=(
-                            "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://assets.calendly.com https://pagead2.googlesyndication.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https://pagead2.googlesyndication.com; font-src 'self' https://fonts.gstatic.com; frame-src https://www.google.com/recaptcha/ https://assets.calendly.com https://calendly.com; connect-src 'self' https://www.google.com/recaptcha/ https://form.cullancarey.com https://form.develop.cullancarey.com; base-uri 'self'; form-action 'self'; upgrade-insecure-requests;"
+                            "default-src 'self'; "
+                            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://assets.calendly.com https://pagead2.googlesyndication.com; "
+                            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
+                            "img-src 'self' data: https://pagead2.googlesyndication.com; "
+                            "font-src 'self' https://fonts.gstatic.com; "
+                            "frame-src 'self' https://www.google.com/recaptcha/ https://assets.calendly.com https://calendly.com https://pagead2.googlesyndication.com; "
+                            "connect-src 'self' https://www.google.com/recaptcha/ https://form.cullancarey.com https://form.develop.cullancarey.com https://pagead2.googlesyndication.com; "
+                            "base-uri 'self'; "
+                            "form-action 'self'; "
+                            "upgrade-insecure-requests;"
                         ),
                         override=True,
                     ),
@@ -93,7 +102,6 @@ class CloudfrontDistribution(Construct):
                     ),
                 ),
             )
-
             self.cf_distribution = cloudfront.Distribution(
                 self,
                 f"WebsiteDistribution",
